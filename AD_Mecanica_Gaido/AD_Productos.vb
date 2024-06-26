1b62625fdd8679390dc2d5994f35112c628d07f3
Fabri-ruizz98 <fabricioruiz668@gmail.com>
2024-06-26 19:08:09 -0300

    FRM marcas/agregar y frm productos/cargar combo

---
+++ AD_Mecanica_Gaido/AD_Productos.vb
@@ -0,0 +1,36 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.IO
+Imports Microsoft.Practices.EnterpriseLibrary.Data
+Imports System.Configuration
+Imports System.Data.Common
+
+Public Class AD_Productos
+    Private connectionString As String
+
+    Public Sub New()
+        connectionString = "Data Source=168.197.51.109;Initial Catalog=PIN_GRUPO31; UID=PIN_GRUPO31; PWD=PIN_GRUPO31123"
+    End Sub
+
+    Public Function Combo_Marcas() As DataTable
+        Dim tabla As New DataTable
+
+        Using conexion As New SqlConnection(connectionString)
+            Using comando As New SqlCommand("Combo_Marcas", conexion)
+                comando.CommandType = CommandType.StoredProcedure
+
+                Try
+                    conexion.Open()
+                    Dim datadapter As New SqlDataAdapter(comando)
+                    datadapter.Fill(tabla)
+                Catch ex As Exception
+                    Throw New Exception("Error al cargar las marcas desde la base de datos", ex)
+                End Try
+
+            End Using
+        End Using
+
+        Return tabla
+    End Function
+End Class
