c9ea5b188ff893208aeb81eff00d7aa61afec468
rocioluque <rocioluq4@gmail.com>
2024-07-11 00:19:41 -0300

    Modificar Rubros

--- Mecanica_Gaido_frm/frmModificarRubros.Designer.vb
+++ Mecanica_Gaido_frm/frmModificarRubros.Designer.vb
@@ -23,7 +23,110 @@ Partial Class frmModificarRubros
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmModificarRubros))
+        Me.txtRubro = New System.Windows.Forms.TextBox()
+        Me.lblCiudad = New System.Windows.Forms.Label()
+        Me.chkEstado = New System.Windows.Forms.CheckBox()
+        Me.grdModificarRubros = New System.Windows.Forms.DataGridView()
+        Me.btnCancelar = New System.Windows.Forms.Button()
+        Me.txtId = New System.Windows.Forms.TextBox()
+        Me.lblNumeroRubro = New System.Windows.Forms.Label()
+        Me.btnModificar = New System.Windows.Forms.Button()
+        CType(Me.grdModificarRubros, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'txtRubro
+        '
+        Me.txtRubro.Font = New System.Drawing.Font("Century Gothic", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtRubro.Location = New System.Drawing.Point(421, 38)
+        Me.txtRubro.Name = "txtRubro"
+        Me.txtRubro.Size = New System.Drawing.Size(187, 21)
+        Me.txtRubro.TabIndex = 43
+        '
+        'lblCiudad
+        '
+        Me.lblCiudad.AutoSize = True
+        Me.lblCiudad.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCiudad.ForeColor = System.Drawing.Color.White
+        Me.lblCiudad.Location = New System.Drawing.Point(322, 39)
+        Me.lblCiudad.Name = "lblCiudad"
+        Me.lblCiudad.Size = New System.Drawing.Size(52, 20)
+        Me.lblCiudad.TabIndex = 44
+        Me.lblCiudad.Text = "Rubro"
+        '
+        'chkEstado
+        '
+        Me.chkEstado.AutoSize = True
+        Me.chkEstado.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.chkEstado.ForeColor = System.Drawing.Color.White
+        Me.chkEstado.Location = New System.Drawing.Point(77, 94)
+        Me.chkEstado.Margin = New System.Windows.Forms.Padding(2)
+        Me.chkEstado.Name = "chkEstado"
+        Me.chkEstado.Size = New System.Drawing.Size(85, 21)
+        Me.chkEstado.TabIndex = 42
+        Me.chkEstado.Text = "¿Activo?"
+        Me.chkEstado.UseVisualStyleBackColor = True
+        '
+        'grdModificarRubros
+        '
+        Me.grdModificarRubros.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.grdModificarRubros.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells
+        Me.grdModificarRubros.BackgroundColor = System.Drawing.Color.FromArgb(CType(CType(65, Byte), Integer), CType(CType(65, Byte), Integer), CType(CType(65, Byte), Integer))
+        Me.grdModificarRubros.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.grdModificarRubros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.grdModificarRubros.Location = New System.Drawing.Point(44, 146)
+        Me.grdModificarRubros.Name = "grdModificarRubros"
+        Me.grdModificarRubros.ReadOnly = True
+        Me.grdModificarRubros.Size = New System.Drawing.Size(747, 221)
+        Me.grdModificarRubros.TabIndex = 41
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.FlatAppearance.BorderColor = System.Drawing.Color.SeaGreen
+        Me.btnCancelar.FlatAppearance.BorderSize = 2
+        Me.btnCancelar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.MediumSeaGreen
+        Me.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnCancelar.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCancelar.ForeColor = System.Drawing.Color.White
+        Me.btnCancelar.Location = New System.Drawing.Point(745, 86)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(82, 29)
+        Me.btnCancelar.TabIndex = 40
+        Me.btnCancelar.Text = "Cancelar"
+        Me.btnCancelar.UseVisualStyleBackColor = True
+        '
+        'txtId
+        '
+        Me.txtId.Font = New System.Drawing.Font("Century Gothic", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtId.Location = New System.Drawing.Point(172, 38)
+        Me.txtId.Name = "txtId"
+        Me.txtId.Size = New System.Drawing.Size(63, 21)
+        Me.txtId.TabIndex = 37
+        '
+        'lblNumeroRubro
+        '
+        Me.lblNumeroRubro.AutoSize = True
+        Me.lblNumeroRubro.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNumeroRubro.ForeColor = System.Drawing.Color.White
+        Me.lblNumeroRubro.Location = New System.Drawing.Point(73, 39)
+        Me.lblNumeroRubro.Name = "lblNumeroRubro"
+        Me.lblNumeroRubro.Size = New System.Drawing.Size(73, 20)
+        Me.lblNumeroRubro.TabIndex = 39
+        Me.lblNumeroRubro.Text = "N° Rubro"
+        '
+        'btnModificar
+        '
+        Me.btnModificar.FlatAppearance.BorderColor = System.Drawing.Color.SeaGreen
+        Me.btnModificar.FlatAppearance.BorderSize = 2
+        Me.btnModificar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.MediumSeaGreen
+        Me.btnModificar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnModificar.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnModificar.ForeColor = System.Drawing.Color.White
+        Me.btnModificar.Location = New System.Drawing.Point(745, 30)
+        Me.btnModificar.Name = "btnModificar"
+        Me.btnModificar.Size = New System.Drawing.Size(82, 29)
+        Me.btnModificar.TabIndex = 38
+        Me.btnModificar.Text = "Modificar"
+        Me.btnModificar.UseVisualStyleBackColor = True
         '
         'frmModificarRubros
         '
@@ -31,11 +134,30 @@ Partial Class frmModificarRubros
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(65, Byte), Integer), CType(CType(65, Byte), Integer), CType(CType(65, Byte), Integer))
         Me.ClientSize = New System.Drawing.Size(871, 396)
+        Me.Controls.Add(Me.txtRubro)
+        Me.Controls.Add(Me.lblCiudad)
+        Me.Controls.Add(Me.chkEstado)
+        Me.Controls.Add(Me.grdModificarRubros)
+        Me.Controls.Add(Me.btnCancelar)
+        Me.Controls.Add(Me.txtId)
+        Me.Controls.Add(Me.lblNumeroRubro)
+        Me.Controls.Add(Me.btnModificar)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "frmModificarRubros"
         Me.Text = "frmModificarRubros"
+        CType(Me.grdModificarRubros, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents txtRubro As TextBox
+    Friend WithEvents lblCiudad As Label
+    Friend WithEvents chkEstado As CheckBox
+    Friend WithEvents grdModificarRubros As DataGridView
+    Friend WithEvents btnCancelar As Button
+    Friend WithEvents txtId As TextBox
+    Friend WithEvents lblNumeroRubro As Label
+    Friend WithEvents btnModificar As Button
 End Class
--- Mecanica_Gaido_frm/frmModificarRubros.vb
+++ Mecanica_Gaido_frm/frmModificarRubros.vb
@@ -1,3 +1,77 @@
-﻿Public Class frmModificarRubros
+﻿Imports System.Data
+Imports AD_Mecanica_Gaido
+Imports System.Data.SqlClient
+Public Class frmModificarRubros
+    Dim o_rubro As New AD_Rubros
+#Region "Procedimientos"
+    Private Sub frmPersonas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        limpiar()
+        Cargar_Grilla()
+    End Sub
 
+    Public Sub limpiar()
+        txtId.Text = Nothing
+        txtRubro.Text = Nothing
+        chkEstado.Checked = False
+    End Sub
+
+    Public Sub Cargar_Grilla()
+        Dim conexion As SqlConnection
+        Dim comando As New SqlCommand
+
+        conexion = New SqlConnection("Data Source=168.197.51.109;Initial Catalog=PIN_GRUPO31; UID=PIN_GRUPO31; PWD=PIN_GRUPO31123")
+
+        conexion.Open()
+        comando.Connection = conexion
+        comando.CommandType = CommandType.StoredProcedure
+        comando.CommandText = ("Cargar_Grilla_Rubro")
+
+        Dim datadapter As New SqlDataAdapter(comando)
+        Dim oDs As New DataSet
+        datadapter.Fill(oDs)
+
+        If oDs.Tables(0).Rows.Count > 0 Then
+            grdModificarRubros.AutoGenerateColumns = True
+            grdModificarRubros.DataSource = oDs.Tables(0)
+            grdModificarRubros.Refresh()
+        End If
+
+        oDs = Nothing
+        conexion.Close()
+    End Sub
+
+    Public Sub CargarDatosEnTextBoxes(ByVal rowIndex As Integer)
+        If grdModificarRubros.Rows.Count > 0 Then
+            txtId.Text = Convert.ToInt32(grdModificarRubros.Rows(rowIndex).Cells("N° Rubro").Value)
+            txtRubro.Text = grdModificarRubros.Rows(rowIndex).Cells("Rubro").Value.ToString()
+            chkEstado.Checked = Convert.ToBoolean(grdModificarRubros.Rows(rowIndex).Cells("¿Está activo?").Value)
+        End If
+    End Sub
+
+    Private Sub grdModificarRubros_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles grdModificarRubros.CellClick
+        If e.RowIndex >= 0 Then
+            CargarDatosEnTextBoxes(e.RowIndex)
+        End If
+    End Sub
+
+    Private Sub btnCancelar_Click(sender As Object, e As EventArgs) Handles btnCancelar.Click
+        limpiar()
+    End Sub
+#End Region
+
+#Region "Modificar"
+    Private Sub btnModificar_Click(sender As Object, e As EventArgs) Handles btnModificar.Click
+        Try
+            Dim RubroID As Integer = txtId.Text
+            Dim nombreRubro As String = txtRubro.Text
+            Dim estado As Boolean = chkEstado.Checked
+
+            o_rubro.Modificar_Rubro(RubroID, nombreRubro, estado)
+            MsgBox("Rubro modificado correctamente.", vbInformation, "Éxito")
+            Cargar_Grilla()
+        Catch ex As Exception
+            MsgBox("Error al modificar el rubro: " & ex.Message, vbCritical, "Error")
+        End Try
+    End Sub
+#End Region
 End Class
