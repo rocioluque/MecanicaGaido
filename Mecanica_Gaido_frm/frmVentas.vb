746243b4e318463d2e4e4c9acdc3b97e6d658f3c
Fabri-ruizz98 <fabricioruiz668@gmail.com>
2024-08-23 18:33:46 -0300

    ..

--- Mecanica_Gaido_frm/frmVentas.vb
+++ Mecanica_Gaido_frm/frmVentas.vb
@@ -241,7 +241,8 @@ Public Class frmVentas
         End If
     End Sub
 #End Region
-    Private Sub btnAceptar_Click(sender As Object, e As EventArgs) Handles btnAceptar.Click
 
-    End Sub
+
+
+
 End Class
